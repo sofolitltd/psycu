8b089df88746f0f9be4591fbf3af9643578e3540
sofolitltd <sofolitltd@gmail.com>
2025-05-09 23:01:02 +0600

    first commit

---
+++ app/alumni/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function SheetPage() {
+  const sheetUrl = process.env.NEXT_PUBLIC_GOOGLE_SHEET_URL; // Access the env variable
+  
+  const router = useRouter();
+
+  useEffect(() => {
+    const isAuthenticated = sessionStorage.getItem('isAuthenticated');
+    if (!isAuthenticated) {
+      router.push('/login');
+    }
+  }, [router]);
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Google Sheet</h1>
+      <iframe
+        src={sheetUrl}
+        className="w-full h-[500px] border-0"
+      ></iframe>
+    </div>
+  );
+}
